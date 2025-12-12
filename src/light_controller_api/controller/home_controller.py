518f612af577ec8ec687fc5781a4b6b08f5d22d4
MaruthiSarathChandra <106025479+MaruthiSarathChandra@users.noreply.github.com>
2025-12-12 16:07:27 -0600

    Delete src directory

--- src/light_controller_api/controller/home_controller.py
+++
@@ -1,7 +0,0 @@
-from flask import current_app, jsonify, Blueprint, render_template
-
-home_bp = Blueprint('home_controller', __name__)
-
-@home_bp.route('/')
-def home():
-    return render_template("home.html")
